f94b2dc7541fa941a7826ccd16b9dd10ba193a00
stira <ralf.stich@infoniqa.com>
2021-02-28 15:52:19 +0100

    some cleanups

--- install.zsh
+++ install.zsh
@@ -1,12 +1,14 @@
+#!/usr/bin/env zsh
+
 # initialize installation packages
-zsh ~/.scripts/install_init.zsh
+~/.scripts/install_init.zsh
 
 # install base desktop packages
-zsh ~/.scripts/install_base.zsh
-zsh ~/.scripts/install_rust_apps.zsh
+~/.scripts/install_base.zsh
+~/.scripts/install_rust_apps.zsh
 
 # install vm solutions
-zsh ~/.scripts/install_vm.zsh
+~/.scripts/install_vm.zsh
 
 # finish installation
-zsh ~/.scripts/install_finish.zsh
+~/.scripts/install_finish.zsh
